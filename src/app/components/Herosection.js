5bee7f8f241f70df4e4229c2dbc05c666b108374
ALIA <alivamallick9@gmail.com>
2023-05-07 01:01:38 +0530

    shape divider

--- src/app/components/Herosection.js
+++ src/app/components/Herosection.js
@@ -1,13 +1,15 @@
 import heroStyles from "@/app/styles/herosection.module.css";
 import styles from "@/app/styles/common.module.css";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import { Mulish } from "next/font/google";
+
 const mulish = Mulish({
   weight: ["300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
   display: "swap",
 });
+
 const Herosection = ({ title, imageUrl }) => {
   return (
     <main className={heroStyles.main_section}>
@@ -25,6 +27,7 @@ const Herosection = ({ title, imageUrl }) => {
               <button className={mulish.className}>Explore Movies</button>
             </Link>
           </div>
+
           <div className={heroStyles.hero_image}>
             <Image
               src={imageUrl}
@@ -34,6 +37,19 @@ const Herosection = ({ title, imageUrl }) => {
             />
           </div>
         </div>
+      </div>
+      <div className={heroStyles["custom-shape-divider-bottom-1681647578"]}>
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            className={heroStyles["shape-fill"]}
+          ></path>
+        </svg>
       </div>
     </main>
   );
